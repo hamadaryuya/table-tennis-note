239d48873e04ffffe706d18fcb1bb55a12beedd3
Ryuya Hamada <m84.hamada.ryuya@gmail.com>
2020-10-09 15:55:58 +0900

    usersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_10_08_101756) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
